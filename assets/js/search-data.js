210b7de067bb00507baa22446480e73c08204cae
tringenb <51790561+tringenb@users.noreply.github.com>
2025-09-25 03:53:14 +0000

    Deploying to gh-pages from @ tringenb/tringenb.github.io@2db1d0ef0b7aa67ee9f241b6e5fc56ec4cd665b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,20 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-research-group",
           title: "Research Group",
           description: "The GUARD (Guarding Users Against Risk and Deception) Research Group at Purdue University studies the human side of cybersecurity, with a focus on social cybersecurity. Our projects range from investigating online child safety and fraud, to understanding vigilante justice, to creating better ways to teach and learn about cybersecurity. We work on a wide variety of projects related to people and their use of technology. Some dig into persuasion and manipulation tactics used in cybercrime, while others create and deliver training for law enforcement or run hands-on workshops for teens. **If you’re interested in joining GUARD, check out our page for prospective students.**",
